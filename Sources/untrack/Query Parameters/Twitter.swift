1c6ffa2f67dc1b5c5c6395c621769273c35f8b3e
Kyle Lanchman <k.lanchman@gmail.com>
2022-05-27 00:20:15 -0400

    Remote Twitter `t` query parameter

--- Sources/untrack/Query Parameters/Twitter.swift
+++ Sources/untrack/Query Parameters/Twitter.swift
@@ -18,6 +18,6 @@ import Foundation
 
 struct TwitterQueryParamRemover: QueryParamRemover {
     func shouldRemove(queryItem: URLQueryItem, from components: URLComponents) -> Bool {
-        return components.host == "twitter.com" && queryItem.name == "s"
+        return components.host == "twitter.com" && ["s", "t"].contains(queryItem.name)
     }
 }
